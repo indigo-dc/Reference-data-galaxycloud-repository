b8ffd4211714f6d19677b88f7f1f4fc234f12108
mtangaro <ma.tangaro@gmail.com>
2017-05-18 15:29:03 +0000

    fix links

--- refdata_download.py
+++ refdata_download.py
@@ -1,5 +1,11 @@
 '''
-comments here
+ELIXIR-ITALY
+INDIGO-DataCloud
+IBIOM-CNR
+
+Contributors:
+author: Tangaro Marco
+email: ma.tangaro@ibbe.cnr.it
 '''
 
 import os
@@ -12,6 +18,7 @@ import pycurl
 GALAXY_USER='galaxy'
 LOG_FILENAME = "/tmp/refdata_download.log"
 
+#______________________________________
 def parse_cli_options():
   parser = argparse.ArgumentParser(description='Download Reference Data', formatter_class=argparse.RawTextHelpFormatter)
   parser.add_argument( '-i', '--input',  dest='genome_list', help='')
@@ -19,25 +26,30 @@ def parse_cli_options():
   parser.add_argument( '-s', '--space', default='elixir-italy.galaxy.refdata', dest='space', help='')
   return parser.parse_args()
 
+#______________________________________
 def load_input_file(refdata_list_file = "genome-list.yml"):
   with open(refdata_list_file, 'r') as fi:
     li = yaml.load(fi)
   return li
 
+#______________________________________
 def clear_log(log):
   with open(log, 'w'):
     pass
 
+#______________________________________
 def create_dir(dirpath = 'directory_path'):
   if not os.path.exists(dirpath):
     os.makedirs(dirpath)
 
+#______________________________________
 def write_data(name, url):
   out = 'Downloading: ' + url
   logging.debug(out)
   try: 
     response = urllib2.urlopen(url)
     with open(name, "wb") as fp:
+      print url
       curl = pycurl.Curl()
       curl.setopt(pycurl.URL, url)
       curl.setopt(pycurl.WRITEDATA, fp)
@@ -48,7 +60,7 @@ def write_data(name, url):
     logging.debug('WARNING: %s NOT FOUND. SKIPPING!!!', url)
     pass
 
-
+#______________________________________
 def download():
   options = parse_cli_options()
   li = load_input_file(options.genome_list)
@@ -65,7 +77,7 @@ def download():
   
   for i in li['genomes']:
     genome_dir = outdir +'/'+i['genome']
-    #print genome_dir
+    print genome_dir
     create_dir(genome_dir)
     os.chdir(genome_dir)
     try:
@@ -73,17 +85,17 @@ def download():
         for j in i['others']:
           url_1 = recas_url + '/' + i['genome'] + '/' + j['other'] 
           print url_1
-          #write_data(j['other'], url_1)
+          write_data(j['other'], url_1)
     except KeyError:
       print 'No others section!'
 
     try:
       if i['tools']:
         for j in i['tools']:
-          #os.chdir(genome_dir)
+          os.chdir(genome_dir)
           tool_dir = genome_dir + '/' + j['tool']
           create_dir(tool_dir)
-
+          print tool_dir
           for k in j['parts']:
             os.chdir(tool_dir)
             url_2 = recas_url+'/'+i['genome']+'/'+j['tool']+'/'+k['part']
@@ -91,10 +103,8 @@ def download():
             write_data(k['part'], url_2)
 
     except KeyError:
-      print 'No tool'
+      print 'No tool!'
 
-
-
-
+#______________________________________
 if __name__ == "__main__":
   download()
