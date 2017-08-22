41d8bbfbfa5b3590a18eb290349d07c726882976
mtangaro <ma.tangaro@gmail.com>
2017-08-22 22:14:06 +0000

    fix path

--- scripts/refdata_download.sh
+++ scripts/refdata_download.sh
@@ -5,5 +5,5 @@ repo_dir="elixir-italy.galaxy.refdata"
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
     echo "List file: $line"
-    python refdata_download.py -i 'lists/'$line -o $cvmfs_dir -s $repo_dir
+    /usr/bin/python refdata_download.py -i '../lists/'$line -o $cvmfs_dir -s $repo_dir
 done < "$1"
